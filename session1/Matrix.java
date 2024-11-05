f466234f48ad63eb292b94bf768919a9db88a246
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-05 19:31:22 +0330

    Create Matrix.java

---
+++ session1/Matrix.java
@@ -0,0 +1,22 @@
+package ir.isc.training1;
+
+public class FirstSession {
+
+	public static void main(String[] args) {
+		printMatrix(12);
+		
+	public static void printMatrix(int n) {
+		
+		 int rows = n;
+	     int columns = n;
+	 
+	     int[][] array = new int[n][n];
+	 
+		for (int i = 0; i < rows; i++) {
+			 for (int j = 0; j < columns; j++) {
+				 System.out.print((int)(Math.random() * 2));
+			}
+			 System.out.println();
+		}
+	}
+}
