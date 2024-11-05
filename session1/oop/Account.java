17010174c78273928b00a71209a095ba34a139c8
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-05 19:39:22 +0330

    Create Account.java

---
+++ session1/oop/Account.java
@@ -0,0 +1,73 @@
+package ir.isc.training1;
+
+import java.time.LocalDateTime;
+
+public class Account {
+	
+	public Account(int balance, String branch) {
+		super();
+		this.balance = balance;
+		this.branch = branch;
+	}
+	
+	public Account(int balance) {
+		super();
+		this.balance = balance;
+	}
+
+
+	private int balance;
+	private String branch;
+	private boolean locked;
+	private LocalDateTime lastTransaction;
+	
+	
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public boolean isLocked() {
+		return locked;
+	}
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+	public LocalDateTime getLastTransaction() {
+		return lastTransaction;
+	}
+	public void setLastTransaction(LocalDateTime lastTransaction) {
+		this.lastTransaction = lastTransaction;
+	}
+	
+	
+	
+	
+	
+	public void print() {
+		System.out.println("Account balance: "+ balance);
+		System.out.println("Account branch: " + branch);
+		System.out.println("Account status: " + locked);
+		System.out.println("Last Transaction: " + lastTransaction);
+		
+	}
+	
+	public int withdraw(int balance) {
+		if (this.balance >= balance) {
+			this.balance -= balance;
+			return this.balance;
+		}
+		else
+			System.out.println("the account does not have sufficient balance! ");
+			return this.balance;
+			
+	}
+
+}
