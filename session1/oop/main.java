506a5d6d04345e9d8ad428a9d64bdc6967aca977
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-05 19:40:48 +0330

    Create main.java

---
+++ session1/oop/main.java
@@ -0,0 +1,11 @@
+package ir.isc.training1;
+import java.time.LocalDateTime;
+public class main {
+
+	public static void main(String[] args) {
+		Account first = new Account(200,"Shariati");
+		first.withdraw(230);
+		first.setLastTransaction(LocalDateTime.now());
+		first.print();
+	}
+}
