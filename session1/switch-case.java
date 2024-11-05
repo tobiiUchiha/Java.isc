82567bc83c4133c1552dfd19324ac8ad46623859
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-05 19:28:22 +0330

    Update switch-case.java

--- session1/switch-case.java
+++ session1/switch-case.java
@@ -6,6 +6,7 @@ import java.math.*;
 public class FirstSession {
 
 	public static void main(String[] args) {
+		// ---- first EX
 		int score = 90;
 		char gradeName;
 		switch ((score >= 90) ? 'A' : (score >= 80) ? 'B' : (score >= 70) ? 'C' : (score >= 60) ? 'D' : 'F') {
@@ -29,6 +30,16 @@ public class FirstSession {
 			gradeName = 'F';
 			System.out.println(gradeName);
 			break;
+		}
+
+
+		// ---- second EX
+		String personName = "Seda";
+		switch (personName) {
+		case "Ali","Mohammad","Reza" -> System.out.println("Welcome men!");
+		case "Fateme","Sevda","Zahra" -> System.out.println("Welcome women!");
+		default -> System.out.println("I don't know who you are!");
+		
 		}
 	}
 }
