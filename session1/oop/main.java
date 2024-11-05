a27da842babdc2cc59a8751b16120b051048426a
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-05 19:42:28 +0330

    Update main.java

--- session1/oop/main.java
+++ session1/oop/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		Account first = new Account(200,"Shariati");
 		first.withdraw(230);
+		first.print();
+		first.withdraw(50);
 		first.setLastTransaction(LocalDateTime.now());
 		first.print();
 	}
