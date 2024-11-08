dd49f4e5de9f38ee64ad9c9e25a577f331a99c3b
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-08 20:22:54 +0330

    Create Main.java

---
+++ session2/Person/Main.java
@@ -0,0 +1,20 @@
+package ir.isc.training1;
+public class main {
+
+	public static void main(String[] args) {
+		
+			Teacher ali = new Teacher();
+			ali.notifyPerson();
+			ali.awake();
+			
+			Student reza = new Student();
+			reza.notifyPerson();
+			reza.awake();
+			
+			Manager mohammad = new Manager();
+			mohammad.notifyPerson();
+			mohammad.awake();
+			
+		
+	}
+}
