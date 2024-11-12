a76cbb35e43e37d8abab9e5dc06e672fe7599a71
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-12 13:39:01 +0330

    Create FileUtilsException.java

---
+++ session4/FileUtilsException.java
@@ -0,0 +1,10 @@
+package ir.isc.training1;
+
+public class FileUtilsException extends Exception {
+
+	public FileUtilsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
+
