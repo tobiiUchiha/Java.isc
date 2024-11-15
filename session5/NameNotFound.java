da77248b6224559be034ef402a802ba168898338
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-15 20:46:59 +0330

    Create NameNotFound.java

---
+++ session5/NameNotFound.java
@@ -0,0 +1,9 @@
+package ir.isc.training1.service.exception;
+
+public class NameNotFound extends Exception {
+
+	public NameNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+}
+}
