0a0010e46d4dcca835b374a997abd25390451043
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-11-27 18:03:40 +0330

    Create EmployeeServiceException.java

---
+++ session8/EmployeeServiceException.java
@@ -0,0 +1,10 @@
+package jsonproject;
+
+public class EmployeeServiceException extends Exception {
+	private static final long serialVersionUID = 6297258431482127104L;
+
+	public EmployeeServiceException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+}
