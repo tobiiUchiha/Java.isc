91050fe2f04f1783195d293deb19680f97b6bd8a
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-12-01 21:34:27 +0330

    Delete withgenericsadvanced2 directory

--- withgenericsadvanced2/Shape.java
+++
@@ -1,6 +0,0 @@
-package com.freeland.oop.session12generic.withgenericsadvanced2;
-
-//Shape.java
-public abstract class Shape {
-	public abstract double area();
-}
--- withgenericsadvanced2/Shape1.java
+++
@@ -1,6 +0,0 @@
-package com.freeland.oop.session12generic.withgenericsadvanced2;
-
-public abstract class Shape1 {
-	abstract float  area();
-	
-}
