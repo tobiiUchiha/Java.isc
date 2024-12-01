391c2271bb16f7832e0c0bef611a56b0dda80b17
Mohammad Modaresi <36224765+tobiiUchiha@users.noreply.github.com>
2024-12-01 21:35:30 +0330

    Delete WithOpenCloseNewTypeFixed directory

--- WithOpenCloseNewTypeFixed/Account.java
+++
@@ -1,7 +0,0 @@
-package com.freeland.oop.session09liskov.WithOpenCloseNewTypeFixed;
-
-import java.math.BigDecimal;
-
-public abstract class Account {
-    protected abstract void deposit(BigDecimal amount);
-}
--- WithOpenCloseNewTypeFixed/FixedTermDepositAccount.java
+++
@@ -1,9 +0,0 @@
-package com.freeland.oop.session09liskov.WithOpenCloseNewTypeFixed;
-
-import java.math.BigDecimal;
-
-public class FixedTermDepositAccount extends Account {
-    protected void deposit(BigDecimal amount) {
-        // Deposit into this account
-    }
-}
